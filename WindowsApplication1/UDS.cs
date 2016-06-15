8de8fc08ef4724361fc452c0de936071ce82a1ac
Saber <cwbhhjl@gmail.com>
2016-06-15 17:55:56 +0800

    删除未使用的using

--- WindowsApplication1/UDS.cs
+++ WindowsApplication1/UDS.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace USBCAN
+﻿namespace USBCAN
 {
     // Negative response codes
     public struct NRC
