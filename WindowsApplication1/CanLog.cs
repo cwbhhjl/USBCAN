6268142718dc79b030228fad7dc744c52cf1ef1c
Nasei <cwbhhjl@gmail.com>
2016-06-22 21:05:40 +0800

    准备log功能

--- WindowsApplication1/CanLog.cs
+++ WindowsApplication1/CanLog.cs
@@ -1,48 +1,49 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace USBCAN
 {
     public class CanLog
     {
         private StreamWriter log = null;
-        private string logStr;
+        private StringBuilder logStr;
 
         public CanLog()
         {
-            logStr = "";
+            logStr = new StringBuilder();
             //log = new StreamWriter(Environment.CurrentDirectory + "Can" + "-" + DateTime.Now.Hour.ToString() + "-" + DateTime.Now.Minute.ToString() + "-" + DateTime.Now.Second.ToString() + ".log", true);
         }
 
         unsafe public void recordLog(VCI_CAN_OBJ obj)
         {
-            logStr += "帧ID:0x" + System.Convert.ToString((int)obj.ID, 16);
-            logStr += "  帧格式:";
+            logStr.Append("帧ID:0x" + Convert.ToString((int)obj.ID, 16));
+            logStr.Append("  帧格式:");
             if (obj.RemoteFlag == 0)
-                logStr += "数据帧 ";
+                logStr.Append("数据帧 ");
             else
-                logStr += "远程帧 ";
+                logStr.Append("远程帧 ");
             if (obj.ExternFlag == 0)
-                logStr += "标准帧 ";
+                logStr.Append("标准帧 ");
             else
-                logStr += "扩展帧 ";
+                logStr.Append("扩展帧 ");
 
             if (obj.RemoteFlag == 0)
             {
-                logStr += "数据: ";
+                logStr.Append("数据: ");
                 byte len = (byte)(obj.DataLen % 9);
 
                 for (byte j = 0; j < len; j++)
                 {
-                    logStr += " " + Convert.ToString(obj.Data[j], 16);
+                    logStr.Append(" " + Convert.ToString(obj.Data[j], 16));
                 }
             }
-            logStr += Environment.NewLine;
+            logStr.Append(Environment.NewLine);
         }
 
         public void makeLog()
         {
-            if (logStr != "")
+            if (logStr.Length == 0)
             {
                 log = new StreamWriter(Environment.CurrentDirectory + "Can" + "-" + DateTime.Now.Hour.ToString() + "-" + DateTime.Now.Minute.ToString() + "-" + DateTime.Now.Second.ToString() + ".log", true);
                 log.WriteLine(logStr);
