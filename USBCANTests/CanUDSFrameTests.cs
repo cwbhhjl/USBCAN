a59489445d1dcd2afa99c419a07b8b4e5a4f072c
Saber <cwbhhjl@gmail.com>
2016-12-16 12:42:02 +0800

    完善CanUDSFrame类

---
+++ USBCANTests/CanUDSFrameTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using USBCAN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace USBCAN.Tests
+{
+    [TestClass()]
+    public class CanUDSFrameTests
+    {
+        CanUDSFrame test;
+        //byte[] tmp;
+        [TestMethod()]
+        public void nextFrameTest()
+        {
+            //Queue<byte[]> q = new Queue<byte[]>();
+            
+            //CanUDSFrameTest();
+            byte[] tmp = { 0x03, 0x10, 0x02, 0x33, 0x11, 0x78, 0xfd, 0x66, 0x11, 0x78, 0xfd, 0x66, 0x11, 0x78, 0xfd, 0x66, 0x11, 0x78, 0xfd, 0x66, 0x09 };
+            test = new CanUDSFrame(tmp);
+            byte[] tmp2 = test.nextFrame();
+            byte[] tmp3 = test.nextFrame();
+             //CollectionAssert.AreEqual(q, test.nextFrame());
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CanUDSFrameTest()
+        {
+            //tmp = { 0x10, 0x02, 0x33 };
+            //test = new CanUDSFrame(tmp);
+        }
+    }
+}
--- WindowsApplication1/UDS.cs
+++ WindowsApplication1/UDS.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace USBCAN
 {
@@ -142,7 +143,6 @@ namespace USBCAN
             get
             {
                 return multiFrames.Count;
-
             }
         }
 
@@ -159,11 +159,95 @@ namespace USBCAN
         {
             length = data.Length;
             multiFrames = dataArrayToCanContent(data);
+            allFrames = new Queue<byte[]>(multiFrames);
+        }
+
+        private byte[] makeSingleFrame(byte[] data)
+        {
+            byte[] tmp = new byte[8];
+            byte len = (byte)data.Length;
+            tmp[0] = len;
+            for (int i = 1; i < 8; i++)
+            {
+                tmp[i] = (i - 1) < len ? data[i - 1] : (byte)0xFF;
+            }
+            return tmp;
+        }
+
+        private byte[] makeFirstFrame(byte[] data)
+        {
+            byte[] tmp = new byte[8];
+            int len = data.Length;
+            tmp[0] = (byte)(((N_PCI.FF.N_PCItype << 4) & 0xF0) | (len >> 8) & 0x0F);
+            tmp[1] = (byte)(len & 0xFF);
+            for (int n = 0; n < 6; n++)
+            {
+                tmp[n + 2] = data[n];
+            }
+            return tmp;
+        }
+
+        private byte[] makeConsecutiveFrame(byte[]data ,int index)
+        {
+            return null;
         }
 
         private Queue<byte[]>dataArrayToCanContent(byte[] data)
         {
-            return null;
+            Queue<byte[]> tmpQ = new Queue<byte[]>();
+            int len = data.Length;
+            if (len <= 7)
+            {
+                tmpQ.Enqueue(makeSingleFrame(data));
+                return tmpQ;
+            }
+            else
+            {
+                if (len > 0xFFF)
+                {
+                    Exception e = new Exception("too long");
+                    throw e;
+                }
+                    
+                int framesNum = calcFramesNum(len);
+                //byte[][] tmp = new byte[framesNum][];
+
+                tmpQ.Enqueue(makeFirstFrame(data));
+
+                int index = 6;
+                byte SN = 0;
+                for (int i = 1; i < framesNum; i++)
+                {
+                    byte[] tmp = new byte[8];
+                    SN++;
+                    if (SN > 0x0F)
+                    {
+                        SN = 0;
+                    }
+                    tmp[0] = (byte)(((N_PCI.CF.N_PCItype << 4) & 0xF0) | SN);
+                    for (int j = 1; j < tmp.Length; j++)
+                    {
+                        tmp[j] = index < len ? data[index] : (byte)0xFF;
+                        index++;
+                    }
+                    
+                    tmpQ.Enqueue(tmp);
+                }
+                return tmpQ;
+            }
+        }
+
+        private int calcFramesNum(int length)
+        {
+            if (length <= 7)
+            {
+                return 1;
+            }
+            else
+            {
+                length -= 6;
+                return (int)Math.Ceiling(((double)length) / 7) + 1;
+            }
         }
     }
 }
