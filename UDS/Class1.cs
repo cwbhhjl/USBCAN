8613a7c45acb20264281ae9163f2c344b0e78bb1
Nasei <cwbhhjl@gmail.com>
2017-05-18 21:48:48 +0800

    依赖dll

---
+++ UDS/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BtFlash.Device;
+
+namespace BtFlash.UDS
+{
+    public class Class1
+    {
+
+    }
+}
