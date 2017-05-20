740c19d17153f6ca91c0b943614f8822c74e0a3c
Nasei <cwbhhjl@gmail.com>
2017-05-20 13:29:46 +0800

    设备库测试

---
+++ DeviceTests/ZlgDeviceTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BtFlash.Device.ZLG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BtFlash.Util;
+
+namespace BtFlash.Device.ZLG.Tests
+{
+    [TestClass()]
+    public class ZlgDeviceTests
+    {
+        private ZlgDevice zlg = new ZlgDevice(HardwareType.VCI_USBCAN2, 0);
+
+        [TestMethod()]
+        public void GetErrorTest()
+        {
+            Assert.AreEqual(zlg.GetError(1), "CAN控制器内部FIFO溢出");
+        }
+
+        [TestMethod()]
+        public void OpenDeviceTest()
+        {
+            if (UsbUtil.HasUsbDevice("Description", "ZLG USBCAN"))
+            {
+                Assert.IsTrue(zlg.OpenDevice());
+                Assert.IsTrue(zlg.CloseDevice());
+            }
+            else
+            {
+                Assert.IsFalse(zlg.OpenDevice());
+            }
+        }
+
+        [TestMethod()]
+        public void CloseDeviceTest()
+        {
+            if (UsbUtil.HasUsbDevice("Description", "ZLG USBCAN"))
+            {
+                Assert.IsTrue(zlg.OpenDevice());
+                Assert.IsTrue(zlg.CloseDevice());
+            }
+            else
+            {
+                Assert.IsFalse(zlg.CloseDevice());
+            }
+        }
+
+        [TestMethod()]
+        public void ReadBoardInfoTest()
+        {
+            VCI_BOARD_INFO info = new VCI_BOARD_INFO();
+            if (UsbUtil.HasUsbDevice("Description", "ZLG USBCAN"))
+            {
+                Assert.IsTrue(zlg.OpenDevice());
+                Assert.IsTrue(zlg.ReadBoardInfo(ref info));
+                Assert.IsTrue(zlg.CloseDevice());
+            }
+            else
+            {
+                Assert.IsFalse(zlg.ReadBoardInfo(ref info));
+            }
+        }
+
+        [TestMethod()]
+        public void ReadErrInfoTest()
+        {
+            uint code;
+            if (UsbUtil.HasUsbDevice("Description", "ZLG USBCAN"))
+            {
+                Assert.IsTrue(zlg.OpenDevice());
+                Assert.IsTrue(zlg.ReadErrInfo(out code));
+                Assert.IsTrue(zlg.CloseDevice());
+            }
+        }
+    }
+}
---
+++ Util/UsbSearch.cs
@@ -0,0 +1,35 @@
+﻿using System.Management;
+
+namespace BtFlash.Util
+{
+    public class UsbUtil
+    {
+        private static ManagementObjectCollection collection = null;
+
+        /// <summary>
+        /// 根据设备属性值判断USB设备是否存在
+        /// </summary>
+        /// <param name="property">USB设备属性</param>
+        /// <param name="value">USB设备属性对应值</param>
+        /// <returns>存在返回<c>true</c>,否则返回<c>false</c></returns>
+        public static bool HasUsbDevice(string property, string value)
+        {
+            if (collection == null)
+            {
+                using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub"))
+                {
+                    collection = searcher.Get();
+                }
+            }
+
+            foreach (var d in collection)
+            {
+                if ((string)d.GetPropertyValue(property) == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
